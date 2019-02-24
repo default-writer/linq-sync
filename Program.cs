6fa543ca1cacdc22866d1f1aff7a6fa13d788fa2
hack2root <me@experimentalcommunity.org>
2019-02-24 21:28:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+namespace linq_sync
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var waitHandle = new AutoResetEvent(true);
+            var result = Enumerable
+                .Range(0, 3)
+                .Select(async (int param) => {
+                    waitHandle.WaitOne();
+                    await TestMethod(param);
+                    waitHandle.Set();
+                }).ToArray();
+            Console.ReadKey();
+        }
+
+        private static async Task<int> TestMethod(int param)
+        {
+            Console.WriteLine($"{param} before");
+            await Task.Delay(50);
+            Console.WriteLine($"{param} after");
+            return param;
+        }
+    }
+
+    public static class StopwatchExtensions
+    {
+        public static void Watch(this Stopwatch stopwatch, string message = "",
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0) =>
+        Console.WriteLine(
+            $"{stopwatch.Elapsed} " +
+            $"{message} " +
+            $"{memberName} " +
+            $"{sourceFilePath}:{sourceLineNumber}");
+    }
+}
---
+++ git-create.ps1
@@ -0,0 +1,7 @@
+& echo "# linq-sync" >> README.md
+& echo "https://stackoverflow.com/questions/54853352/linq-select-analogue-for-async-method/54854164#54854164" >> README.md
+& git init
+& git add .
+& git commit -m "first commit"
+& git remote add origin https://github.com/hack2root/linq-sync.git
+& git push -u origin master
